973cfc5d6d1c02aa7f9f89182a967cec1a38c791
Sumit Shrivastava <shrivsk@gmail.com>
2021-01-09 23:27:24 +0000

    deleted some not required files

--- tsc/support/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// ***********************************************************
-// Import commands.js using ES2015 syntax:
-require("./commands");
-require("./utils");
-require("@shelex/cypress-allure-plugin");
-require('@shelex/cypress-allure-plugin');
