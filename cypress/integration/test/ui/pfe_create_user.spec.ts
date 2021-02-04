ca889ca84d36433645a8689e2dfab7bfadf7d8da
Sumit Shrivastava <shrivsk@gmail.com>
2021-02-04 00:26:02 +0000

    update on 4th feb 2021

--- cypress/integration/test/ui/pfe_create_user.spec.ts
+++ cypress/integration/test/ui/pfe_create_user.spec.ts
@@ -4,10 +4,10 @@ import { pfeLoginPage } from '../../pageObjects/pfepages/login-Page'
 // ***********************************************
 let ccdCaseId
 // ***********************************************
-describe('Successfully create PFE/RFE user', () => {
+describe('create PFE/RFE user', () => {
   const pfeCreateUser = new pfeLoginPage()
 
-  it('Successfully create PFE/RFE user', async () => {
+  it('Should create PFE/RFE user', async () => {
     ccdCaseId = pfeCreateUser.createAatIdamUser()
   })
 })
